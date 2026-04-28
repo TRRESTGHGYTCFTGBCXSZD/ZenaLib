2d2ef2355e7826ea3c07d14c4cd738502e6fec7b
TRRESTGHGYTCFTGBCXSZD <40109698+TRRESTGHGYTCFTGBCXSZD@users.noreply.github.com>
2026-04-28 20:38:15 +0900

    new recipe actions

--- ZenaLib/ZenaRecipe.cs
+++ ZenaLib/ZenaRecipe.cs
@@ -13,57 +13,91 @@ namespace ZenaLib;
 
 public abstract class Recipe
 {
-    protected AtomType[] Output;
-    public AtomType[] GetOutput() {
-        return Output;
-    }
+	protected AtomType[] Output;
+	public AtomType[] GetOutput() {
+		return Output;
+	}
+	public abstract bool CheckRecipe(AtomType[] Ingredients);
 }
 public class UnshapedRecipe : Recipe
 {
-    protected Dictionary<AtomType,int> Input;
-    public Dictionary<AtomType,int> GetInput() {
-        return Input;
-    }
-    public UnshapedRecipe(Dictionary<AtomType,int> Targ,AtomType[] Dispel){
-        Input = Targ;
-        Output = Dispel;
-    }
+	protected Dictionary<AtomType,int> Input;
+	public Dictionary<AtomType,int> GetInput() {
+		return Input;
+	}
+	public override bool CheckRecipe(AtomType[] Ingredients){ //partital match unsupported
+		Dictionary<AtomType,int> Dim = new(Input);
+		foreach (AtomType atoms in Ingredients){
+			if (atoms is null) {
+				continue;
+			} else if (Dim.ContainsKey(atoms)) {
+				Dim[atoms]--;
+				if (Dim[atoms] <= 0) {
+					Dim.Remove(atoms);
+				}
+			} else {
+				return false;
+			}
+		}
+		return Dim.Count <= 0;
+	}
+	public UnshapedRecipe(Dictionary<AtomType,int> Targ,AtomType[] Dispel){
+		Input = Targ;
+		Output = Dispel;
+	}
 }
 public class ShapedRecipe : Recipe
 {
-    protected MultipleMatcher[] Input;
-    public MultipleMatcher[] GetInput() {
-        return Input;
-    }
-    public ShapedRecipe(MultipleMatcher[] Targ, AtomType[] Dispel){
-        Input = Targ;
-        Output = Dispel;
-    }
+	protected MultipleMatcher[] Input;
+	public MultipleMatcher[] GetInput() {
+		return Input;
+	}
+	public override bool CheckRecipe(AtomType[] Ingredients){ //partital match unsupported
+		MultipleMatcher[] Dim = Input;
+		if (Dim.Length != Ingredients.Length) {
+			return false;
+		}
+		for (int index = 0;index <= Dim.Length;index++){
+			if (Ingredients[index] is not null && Dim[index] is not null) {
+				if (Dim[index] != Ingredients[index])
+				{
+					return false;
+				}
+			} else if (!(Ingredients[index] is null && Dim[index] is null)){
+				return false;
+			}
+		}
+		return true;
+	}
+	public ShapedRecipe(MultipleMatcher[] Targ, AtomType[] Dispel){
+		Input = Targ;
+		Output = Dispel;
+	}
 }
 public class MultipleMatcher
 {
-    protected AtomType[] Matchers;
-    public MultipleMatcher(AtomType[] Dispel){
-        Matchers = Dispel;
-    }
-    public MultipleMatcher(AtomType Dispel){
-        Matchers = new AtomType[1]{Dispel};
-    }
-    public static bool operator ==(MultipleMatcher a, AtomType b){
-        foreach (AtomType atem in a.Matchers){
-            if (atem == b) {
-                return true;
-            }
-        }
-        return false;
-    }
-    public static bool operator !=(MultipleMatcher a, AtomType b){
-        return !(a == b);
-    }
-    public static bool operator ==(MultipleMatcher a, MultipleMatcher b){
-        return a.GetHashCode() == b.GetHashCode();
-    }
-    public static bool operator !=(MultipleMatcher a, MultipleMatcher b){
-        return !(a == b);
-    }
+	protected AtomType[] Matchers;
+	public MultipleMatcher(AtomType[] Dispel){
+		Matchers = Dispel;
+	}
+	public MultipleMatcher(AtomType Dispel){
+		Matchers = new AtomType[1]{Dispel};
+	}
+	public static bool operator ==(MultipleMatcher a, AtomType b){
+		foreach (AtomType atem in a.Matchers){
+			if (atem == b) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public static bool operator !=(MultipleMatcher a, AtomType b){
+		return !(a == b);
+	}
+	public static bool operator ==(MultipleMatcher a, MultipleMatcher b){
+		return a.GetHashCode() == b.GetHashCode();
+	}
+	public static bool operator !=(MultipleMatcher a, MultipleMatcher b){
+		return !(a == b);
+	}
 }
--- ZenaLib/ZenaRecipeManager.cs
+++ ZenaLib/ZenaRecipeManager.cs
@@ -13,57 +13,15 @@ namespace ZenaLib;
 
 public static class RecipeManager
 {
-    public static bool CheckUnshapedRecipe(AtomType[] Input, UnshapedRecipe Recipe, out AtomType[] Output){ //partital match unsupported
-        Dictionary<AtomType,int> Dim = new(Recipe.GetInput());
-        Output = Recipe.GetOutput();
-        foreach (AtomType atoms in Input){
-            if (atoms is null) {
-                continue;
-            } else if (Dim.ContainsKey(atoms)) {
-                Dim[atoms]--;
-                if (Dim[atoms] <= 0) {
-                    Dim.Remove(atoms);
-                }
-            } else {
-                return false;
-            }
-        }
-        return Dim.Count <= 0;
-    }
-    public static bool CheckShapedRecipe(AtomType[] Input, ShapedRecipe Recipe, out AtomType[] Output){ //partital match unsupported
-        MultipleMatcher[] Dim = Recipe.GetInput();
-        Output = Recipe.GetOutput();
-        if (Dim.Length != Output.Length) {
-            return false;
-        }
-        for (int index = 0;index <= Dim.Length;index++){
-            if (Input[index] is not null && Dim[index] is not null) {
-                if (Dim[index] != Input[index])
-                {
-                    return false;
-                }
-            } else if (!(Input[index] is null && Dim[index] is null)){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static bool CheckRecipes(AtomType[] Input, Recipe[] Recipe, out AtomType[] Output){ //partital match unsupported
-        foreach (Recipe EachRecipe in Recipe){
-            if (EachRecipe is ShapedRecipe Shape){
-                if (CheckShapedRecipe(Input,Shape,out AtomType[] Outo)){
-                    Output = Outo;
-                    return true;
-                }
-            }
-            if (EachRecipe is UnshapedRecipe Freeform){
-                if (CheckUnshapedRecipe(Input,Freeform,out AtomType[] Outo)){
-                    Output = Outo;
-                    return true;
-                }
-            }
-        }
-        Output = new AtomType[0];
-        return false;
-    }
+	public static bool CheckRecipes(AtomType[] Input, Recipe[] Recipe, out AtomType[] Output){
+		foreach (Recipe EachRecipe in Recipe){
+			
+			if (EachRecipe.CheckRecipe(Input)){
+				Output = EachRecipe.GetOutput();
+				return true;
+			}
+		}
+		Output = new AtomType[0];
+		return false;
+	}
 }
